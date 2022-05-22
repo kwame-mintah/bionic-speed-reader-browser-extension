687d421f380d407f89a5768e0a681143b3e04328
Fawaz Mohamed <fwz.mhmd@gmail.com>
2022-05-22 19:39:19 +0100

    Fix button select on highlight

--- src/js/options.js
+++ src/js/options.js
@@ -1,62 +1,64 @@
 /**
  * Request Body parameters, used for requesting preview update.
  */
-var fixation;
-var saccade;
-var apiKey;
+let fixation;
+let saccade;
+let apiKey;
 
 /**
  * User input options
  */
-var inputFixation;
-var inputSaccade;
-var inputApiKey = document.getElementById("apiKey");
+let inputFixation;
+let inputSaccade;
+const inputApiKey = document.getElementById('apiKey');
 
 /**
  * Listen for click event, to initiate saving of the users options.
  */
-var btnSubmit = document.getElementById("submit");
-btnSubmit.addEventListener("click", saveUserOptions);
+const btnSubmit = document.getElementById('submit');
+btnSubmit.addEventListener('click', saveUserOptions);
 
 /**
  * Listen for click event, to initiate POST request to update preview.
  */
-var btnPreview = document.getElementById("preview");
-btnPreview.addEventListener("click", previewChanges);
+const btnPreview = document.getElementById('preview');
+btnPreview.addEventListener('click', previewChanges);
 
 /**
  * Listen for click event, to clear local extension storage
  */
-var btnClear = document.getElementById("clear");
-btnClear.addEventListener("click", clearLocalStorage)
+/**
+ * Clear extension local storage
+ */
+const btnClear = document.getElementById('clear');
+btnClear.addEventListener('click', () => chrome.storage.local.clear());
 
 /**
  * Listen for events on Fixation options, if user clicks a button get the value,
  * of the button clicked.
  */
-const fixationOptions = document.getElementById("fixationOption");
-fixationOptions.addEventListener("click", (Event) => {
-  const btnClicked = Event.target.nodeName === "BUTTON";
+const fixationOptions = document.getElementById('fixationOption');
+fixationOptions.addEventListener('click', e => {
+  const btnClicked = e.target.nodeName === 'BUTTON';
   if (!btnClicked) {
     return;
   }
-  inputFixation = document.getElementById(Event.target.id).value;
-  changeButtonColor(inputFixation);
+  inputFixation = document.getElementById(e.target.id).value;
+  changeButtonColor(e);
 });
-
 
 /**
  * Listen for events on Saccade options, if user clicks a button get the value,
  * of the button clicked.
  */
-const saccadeOptions = document.getElementById("saccadeOption");
-saccadeOptions.addEventListener("click", (Event) => {
-  const btnClicked = Event.target.nodeName === "BUTTON";
+const saccadeOptions = document.getElementById('saccadeOption');
+saccadeOptions.addEventListener('click', e => {
+  const btnClicked = e.target.nodeName === 'BUTTON';
   if (!btnClicked) {
     return;
   }
-  inputSaccade = document.getElementById(Event.target.id).value;
-  changeButtonColor(inputSaccade);
+  inputSaccade = document.getElementById(e.target.id).value;
+  changeButtonColor(e);
 });
 
 /**
@@ -66,18 +68,18 @@ saccadeOptions.addEventListener("click", (Event) => {
  * RapidAPI key if it is not empty or undefined.
  */
 function displayCurrentOptions() {
-  chrome.storage.local.get(["fixation"], function (result) {
+  chrome.storage.local.get(['fixation'], function (result) {
     fixation = result.fixation;
     changeButtonColor(fixation);
   });
-  chrome.storage.local.get(["saccade"], function (result) {
+  chrome.storage.local.get(['saccade'], function (result) {
     saccade = result.saccade;
     changeButtonColor(saccade);
   });
-  chrome.storage.local.get(["apiKey"], function (result) {
+  chrome.storage.local.get(['apiKey'], function (result) {
     apiKey = result.apiKey;
-    if (apiKey != "" && apiKey != undefined){
-      inputApiKey.value = apiKey
+    if (apiKey != '' && apiKey != undefined) {
+      inputApiKey.value = apiKey;
     }
   });
 }
@@ -93,7 +95,7 @@ function saveUserOptions() {
   if (inputSaccade != undefined) {
     chrome.storage.local.set({ saccade: inputSaccade });
   }
-  if (inputApiKey != undefined && inputApiKey.value !== "undefined"){
+  if (inputApiKey != undefined && inputApiKey.value !== 'undefined') {
     chrome.storage.local.set({ apiKey: inputApiKey.value });
   }
   displayCurrentOptions();
@@ -104,23 +106,30 @@ function saveUserOptions() {
  * response is shown on the page.
  */
 function previewChanges() {
-  var content = document.getElementById("previewText").innerHTML;
-  requestBionic(apiKey, content, fixation, saccade)
+  const content = document.getElementById('previewText').innerHTML;
+  requestBionic(apiKey, content, fixation, saccade);
 }
 
 /**
- * Clear extension local storage
- */
-function clearLocalStorage(){
-  chrome.storage.local.clear()
-}
-
-/**
- * Change the <button> color to green.
+ * Change the <button> color to green on select.
  * @param {String} btnId
  */
 function changeButtonColor(btnId) {
-  document.getElementById("btn" + btnId).style.background = '#07ff42';
+  const btnParent = btnId.target.parentElement.parentElement;
+
+  if (btnParent.id === 'fixationOption') {
+    document.querySelectorAll('#fixationOption button').forEach(b => {
+      b.style.background = '';
+    });
+    btnId.target.style.background = '#07ff42';
+  }
+
+  if (btnParent.id === 'saccadeOption') {
+    document.querySelectorAll('#saccadeOption button').forEach(b => {
+      b.style.background = '';
+    });
+    btnId.target.style.background = '#07ff42';
+  }
 }
 
 /**
