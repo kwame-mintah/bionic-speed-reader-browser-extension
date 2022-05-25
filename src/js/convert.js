f6d15b9cc5bbf162125c51eca2f8e465b645e89f
Eddie Cohen <31940792+edcohen08@users.noreply.github.com>
2022-05-25 08:49:37 -0400

    Feature/use url for convert (#19)
    * using url and parsing for converting webpage to Bionic Reading Format
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/js/convert.js
+++ src/js/convert.js
@@ -1,19 +1,29 @@
 /**
- * Find all elements with the p tag and for each one,
- * get the inner text for that tag, to be used as the content
- * for the post request to get the text in Bionic Reading format.
- *
+ * Make a request to convert the entire page into Bionic Reading format
+ * convert the response into a DOM tree and get all of the p tags
+ * get all of the p tags on the page originally and
+ * replace the innerHTML with the innerHTML of the corresponding response p tag
  */
 async function convertPage() {
   let apiKey = await readLocalStorage('apiKey');
   let fixation = await readLocalStorage('fixation');
   let saccade = await readLocalStorage('saccade');
 
+  const parser = new DOMParser();
+  const response = await requestBionic(
+    apiKey,
+    document.URL,
+    fixation,
+    saccade,
+    true
+  );
+  const responseText = parser
+    .parseFromString(response, "text/html")
+    .getElementsByTagName("p");
+
   let arrayText = document.getElementsByTagName("p");
   for (let i = 0; i < arrayText.length; i++) {
-    let innerText = arrayText[i].innerText;
-    let text = await requestBionic(apiKey, innerText, fixation, saccade, true)
-    arrayText[i].innerHTML = text;
+    arrayText[i].innerHTML = responseText[i].innerHTML;
   }
 }
 
